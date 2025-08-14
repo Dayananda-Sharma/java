436d48bf547766b6550002eda9198f30a8fee560
Dayananda-Sharma <dayanandsharma.x-workz@outlook.com>
2025-08-14 19:16:01 +0530

    BirdsRunner.java

---
+++ BirdsCodeRunner.java
@@ -0,0 +1,26 @@
+class BirdsCodeRunner{
+	public static void main (String [] args){
+		int stateBirds = BirdsCode.code(1);
+		System.out.println("Parrot StateCode="+stateBirds);
+		stateBirds = BirdsCode.code(2);
+		System.out.println("Chicken StateCode="+stateBirds);
+		stateBirds = BirdsCode.code(3);
+		System.out.println("Owl StateCode="+stateBirds);
+		stateBirds = BirdsCode.code(4);
+		System.out.println("Blue StateCode="+stateBirds);
+		stateBirds = BirdsCode.code(5);
+		System.out.println("Penguins StateCode="+stateBirds);
+		stateBirds = BirdsCode.code(6);
+		System.out.println("Finches StateCode="+stateBirds);
+		stateBirds = BirdsCode.code(7);
+		System.out.println("Bulebird StateCode="+stateBirds);
+		stateBirds = BirdsCode.code(8);
+		System.out.println("Falcon StateCode="+stateBirds);
+		stateBirds = BirdsCode.code(9);
+		System.out.println("Herons StateCode="+stateBirds);
+		stateBirds = BirdsCode.code(10);
+		System.out.println("Ostrich StateCode="+stateBirds);
+		stateBirds = BirdsCode.code(11);
+		System.out.println("Swans StateCode="+stateBirds);
+	}
+}
