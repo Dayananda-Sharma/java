f1ee93bff91083de5bdb2bb175b576e733b401dc
Dayananda-Sharma <dayanandsharma.x-workz@outlook.com>
2025-09-03 13:09:01 +0530

    if-ifelse-else

---
+++ if-ifelse-else/GaneshaIdol.java
@@ -0,0 +1,203 @@
+class GaneshaIdol{
+  public static int Price (int amount){
+	int money = 0;
+  if(amount == 2000){
+	  money = 1900;
+  System.out.println("---------------yes have that price-------------------");
+  }
+  
+  else  if ( amount == 3000){
+  System.out.println(" -------------------we have -------------------");
+  }
+  else {
+  System.out.println(" we don't have 3000 above");
+    }
+  
+  
+  
+  if(amount == 1700){
+	  money = 2100;
+  System.out.println("------------------7 feet-------------------");
+  }
+  
+  else  if ( amount == 70000){
+  System.out.println("------------------- 15 feet we have------------------- ");
+  }
+  else {
+   System.out.println(" don't have");
+  }
+	
+	
+	
+	
+  if(amount == 5000){
+	  money = 5500;
+  System.out.println("-------------------7 feet-------------------");
+  }
+  
+  else  if ( amount == 10000){
+  System.out.println("------------------- 15 feet we have------------------- ");
+  }
+  else {
+   System.out.println(" we don't ");
+  }
+  
+  
+  
+  if(amount == 6000){
+	  money = 6500;
+  System.out.println("-------------------7 feet-------------------");
+  }
+  
+  else  if ( amount == 11000){
+  System.out.println(" -------------------15 feet we have------------------- ");
+  }
+  else {
+   System.out.println("No");
+  }
+  
+  
+  
+  
+  if(amount == 7000){
+	  money = 7500;
+  System.out.println("-------------------7 feet-------------------");
+  }
+  
+  else  if ( amount == 12000){
+  System.out.println("------------------- 15 feet we have------------------- ");
+  }
+  else {
+   System.out.println(" i can't find");
+  }
+  
+  
+  
+  
+  if(amount == 8000){
+	  money = 8500;
+  System.out.println("-------------------7 feet-------------------");
+  }
+  
+  else  if ( amount == 13000){
+  System.out.println("------------------- 15 feet we have------------------- ");
+  }
+  else {
+   System.out.println(" No idea");
+  }
+  
+  
+  
+  if(amount == 9000){
+	  money = 9500;
+  System.out.println("-------------------7 feet-------------------");
+  }
+  
+  else  if ( amount == 14000){
+  System.out.println("------------------- 15 feet we have -------------------");
+  }
+  else {
+   System.out.println(" i don't have");
+  }
+  
+  
+  
+  if(amount == 20000){
+	  money = 20500;
+  System.out.println("-------------------7 feet-------------------");
+  }
+  
+  else  if ( amount == 15000){
+  System.out.println(" -------------------15 feet we have -------------------");
+  }
+  else {
+   System.out.println("we don't know");
+  }
+  
+  
+  if(amount == 21000){
+	  money = 21500;
+  System.out.println("-------------------7 feet-------------------");
+  }
+  
+  else  if ( amount == 16000){
+  System.out.println("------------------- 15 feet we have------------------- ");
+  }
+  else {
+   System.out.println(" i'll get back you");
+  }
+  
+  
+  
+  if(amount == 22000){
+	  money = 22500;
+  System.out.println("-------------------7 feet-------------------");
+  }
+  
+  else  if ( amount == 17000){
+  System.out.println(" -------------------15 feet we have------------------- ");
+  }
+  else {
+   System.out.println(" i have no");
+  }
+  
+  
+ 
+  if(amount == 23000){
+	  money = 23500;
+  System.out.println("-------------------7 feet-------------------");
+  }
+  
+  else  if ( amount == 18000){
+  System.out.println(" -------------------15 feet we have------------------- ");
+  }
+  else {
+   System.out.println("i have idea");
+  }
+
+
+  
+  if(amount == 24000){
+	  money = 24500;
+  System.out.println("-------------------7 feet-------------------");
+  }
+  
+  else  if ( amount == 19000){
+  System.out.println("------------------- 15 feet we have------------------- ");
+  }
+  else {
+   System.out.println("i think dont have");
+  }
+
+
+
+  if(amount == 25000){
+	  money = 25500;
+  System.out.println("-------------------7 feet-------------------");
+  }
+  
+  else  if ( amount == 20000){
+  System.out.println("------------------- 15 feet we have------------------- ");
+  }
+  else {
+   System.out.println(" we that dont have");
+  }
+
+
+
+  if(amount == 26000){
+	  money = 26500;
+  System.out.println("-------------------7 feet-------------------");
+  }
+  
+  else  if ( amount == 21000){
+  System.out.println(" -------------------15 feet we have -------------------");
+  }
+  else {
+   System.out.println(" i'm not Sure");
+ 
+  }
+ return money;
+  }
+}	
+
---
+++ if-ifelse-else/GaneshaIdolRunner.java
@@ -0,0 +1,7 @@
+class GaneshaIdolRunner{
+	public static void main (String [] args){
+		int money = GaneshaIdol.Price(7000);
+		
+		System.out.println(money);
+	}
+}
